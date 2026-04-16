603a1e4149fba7bc7bf37b3f666de32716c0bc9b
Emmanuel B <blancocemmanuel@gmail.com>
2026-04-16 17:38:11 +1000

    YES

--- src/components/CopyCat.test.jsx
+++ src/components/CopyCat.test.jsx
@@ -14,7 +14,7 @@ expect(header).toHaveTextContent("Copy Cat Mack");
 
 
 it("Should display input text in paragraph when isCopying is set to true", () => {
-render(<CopyCat name={"Mack"} value={"Here is an Input"} handleChange={()=>{}} toggleTape={()=>{}} isCopying={true} />); // The props you pass when you render the component in your test are only for that specific test instance. The test is isolated from the real app. so value is not available as value in the test. 
+render(<CopyCat name={"Mack"} value={"Here is an Input"} handleChange={()=>{}} toggleTape={()=>{}} isCopying={true} />); // The props you pass when you render the component in your test are only for that specific test instance. The test is isolated from the real app. so value is not available as value in the actual test below, just in the props. 
 
 // test the simultaneous feedback from textbox to P
 
@@ -24,9 +24,20 @@ render(<CopyCat name={"Mack"} value={"Here is an Input"} handleChange={()=>{}} t
  // verify that the rendered P has the text "here is an input"
  const paragraph = screen.getByText("Here is an Input");
  expect(paragraph).toBeInTheDocument(); // toBeInDocument does not accept arguments
-
 });
 
+
+
+
 it("Should not display input text in paragraph when isCopying is set to false", () => {
+render(<CopyCat name= {'Mack'} value={"here is another input"} handleChange={()=> {}} toggleTape={()=>{}} isCopying={false}/>);
+
+// verify there is no p set with the text in the input when isCopying is set t false
+
+const inputNode = screen.getByRole("textbox");
+expect(inputNode).toHaveValue("here is another input");
+// verify that there is no  P with text as the isCopying value is set to false. 
+const paragraph = screen.queryByText("here is another input");
+expect(paragraph).not.toBeInTheDocument();
 
 });
